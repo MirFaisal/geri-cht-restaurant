f31af8157bab1d6cf45c02be90d3f0f645b62f79
mir_faisal <mirfaisal08@gmail.comm>
2023-02-01 07:18:33 +0600

    navbar and banner section complete

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(".banner-slide").slick({
+  speed:50,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  autoplay: true,
+  infinite: true,
+  arrows:false,
+  cssEase: "linear",
+});
