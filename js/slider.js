d138e80e01b71421ee138d4df920363b51673943
mir_faisal <mirfaisal08@gmail.comm>
2023-02-06 16:56:18 +0600

    added cefs section

--- js/slider.js
+++ js/slider.js
@@ -9,3 +9,13 @@ $(".banner-slide").slick({
   arrows:false,
   cssEase: "linear",
 });
+$(".chef-slider").slick({
+  speed: 50,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  autoplay: true,
+  infinite: true,
+  cssEase: "linear",
+  arrows: false,
+});
