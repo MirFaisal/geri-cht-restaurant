ca10e7fa2c6347861bdc423cf78c09e007bfc0de
mir_faisal <mirfaisal08@gmail.comm>
2023-02-12 21:41:08 +0600

    fixing baground image

--- js/menu.js
+++ js/menu.js
@@ -7,14 +7,14 @@ var background_2 = document.querySelector(".background-2");
 var background_3 = document.querySelector(".background-3");
 
 riceElement.addEventListener('mouseover', function () {
-    background_1.style.backgroundImage = 'url(images/food/rice-3.png)';
+    background_1.style.background = 'url(images/food/rice-3.png)';
 
 });
 pestyElement.addEventListener("mouseover", function () {
-  background_1.style.backgroundImage = 'url(images/food/pesty.png)';
+  background_1.style.background = 'url(images/food/pesty.png)';
 });
 chocolateElement.addEventListener("mouseover", function () {
-  background_1.style.backgroundImage = 'url(images/food/chocolate.png)';
+  background_1.style.background = 'url(images/food/chocolate.png)';
 });
 riceElement.addEventListener('mouseleave', function () {
     background_1.style.backgroundImage =null;
