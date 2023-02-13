80a3daa84404c9c6d1c65f55c8881b2f97f076fe
mir_faisal <mirfaisal08@gmail.comm>
2023-02-13 16:56:07 +0600

    lerger display design complete

--- js/slider.js
+++ js/slider.js
@@ -30,3 +30,34 @@ $(".testimony-slide").slick({
   arrows: false,
   dots: true,
 });
+
+$(".photo-slide").slick({
+  centerMode: true,
+  slidesToShow: 1,
+  variableWidth: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  arrows: false,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
